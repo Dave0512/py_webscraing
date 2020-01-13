c86b990a9a870dffe42f0df9d3970ce979286ebb
Dave0512 <dschneiderhoffmann@gmail.com>
2020-01-13 10:46:08 +0100

    Create bs object

--- web_scraping.py
+++ web_scraping.py
@@ -2,3 +2,18 @@
 
 import bs4 as bs
 import urllib.request
+
+sauce = urllib.request.urlopen('https://wiki.ubuntuusers.de/pdftk/').read()
+
+# Bs object
+soup = bs.BeautifulSoup(sauce,'lxml') # lxml = parser
+
+# Show html
+# print(soup)
+
+# Show title of doc
+print(soup.title.string)
+print(soup.title.string)
+
+# for url in soup.find_all('a'):
+#     print(url.get('href'))
